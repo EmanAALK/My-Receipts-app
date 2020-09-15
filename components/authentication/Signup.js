ca0988da8cd18c166c6df1a8ab35e01014236b5c
alo-faith <alo.faith@gmail.com>
2020-09-15 19:35:26 +0300

    add user

---
+++ components/authentication/Signup.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import { observer } from "mobx-react";
+
+//store
+import authStore from "../../store/authStore";
+
+//styles
+import {
+  AuthContainer,
+  AuthTitle,
+  AuthTextInput,
+  AuthButton,
+  AuthButtonText,
+  AuthOther,
+} from "./styles";
+
+const Signup = () => {
+  const [user, setUser] = useState({});
+
+  const handleSubmit = async () => {
+    await authStore.signup(user);
+    // if (authStore.user) navigation.replace("FolderList");
+  };
+
+  return (
+    <AuthContainer>
+      <AuthTitle>Sign up</AuthTitle>
+      <AuthTextInput
+        onChangeText={(username) => setUser({ ...user, username })}
+        placeholder="Username"
+        placeholderTextColor="#A6AEC1"
+        autoCapitalize="none"
+      />
+      <AuthTextInput
+        onChangeText={(firstName) => setUser({ ...user, firstName })}
+        placeholder="First name"
+        placeholderTextColor="#A6AEC1"
+      />
+      <AuthTextInput
+        onChangeText={(lastName) => setUser({ ...user, lastName })}
+        placeholder="Last Name"
+        placeholderTextColor="#A6AEC1"
+      />
+      <AuthTextInput
+        onChangeText={(email) => setUser({ ...user, email })}
+        placeholder="Email"
+        placeholderTextColor="#A6AEC1"
+      />
+      <AuthTextInput
+        onChangeText={(password) => setUser({ ...user, password })}
+        placeholder="Password"
+        placeholderTextColor="#A6AEC1"
+        secureTextEntry={true}
+      />
+      <AuthButton onPress={handleSubmit}>
+        <AuthButtonText>Sign up</AuthButtonText>
+      </AuthButton>
+      <AuthOther onPress={() => navigation.navigate("Signin")}>
+        Click here to Sign in
+      </AuthOther>
+    </AuthContainer>
+  );
+};
+
+export default observer(Signup);
