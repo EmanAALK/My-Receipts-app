2da6800a92841f67223c2f8a221a6708b6996b32
EmanAALK <emanalkandarii@gmail.com>
2020-09-17 15:25:57 +0300

    LinearGradient Dependency

--- components/authentication/Signin.js
+++ components/authentication/Signin.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { observer } from "mobx-react";
+
+//Navigation
 import RootNavigator from "../../components/Navigation/RootNavigator";
 
-// Store
+// Stores
 import authStore from "../../store/authStore";
 
+//Styles
 import {
   AuthContainer,
   AuthTitle,
@@ -14,8 +17,9 @@ import {
   AuthOther,
 } from "./styles";
 import Feather from "react-native-vector-icons/Feather";
-import * as Animatable from "react-native-animatable";
+import { LinearGradient } from "expo-linear-gradient";
 import { Text } from "native-base";
+import * as Animatable from "react-native-animatable";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
 const Signin = ({ navigation }) => {
@@ -40,37 +44,51 @@ const Signin = ({ navigation }) => {
   };
   return (
     <AuthContainer>
-      <AuthTitle>Sign in</AuthTitle>
+      <AuthTitle>Welcome!</AuthTitle>
       <AuthTextInput
         onChangeText={(username) => setUser({ ...user, username })}
-        placeholder="Username"
-        placeholderTextColor="#A6AEC1"
-        autoCapitalize="none"
+        placeholder='Username'
+        placeholderTextColor='#A6AEC1'
+        autoCapitalize='none'
         value={user.username}
       />
       <AuthTextInput
         onChangeText={(password) => setUser({ ...user, password })}
-        placeholder="Password"
-        placeholderTextColor="#A6AEC1"
+        placeholder='Password'
+        placeholderTextColor='#A6AEC1'
         secureTextEntry={showPass ? false : true}
         value={user.password}
       />
       <TouchableOpacity onPress={toggleShowPass}>
-        <Feather name={showPass ? "eye" : "eye-off"} color="grey" size={15} />
+        <Feather name={showPass ? "eye" : "eye-off"} color='grey' size={15} />
       </TouchableOpacity>
+      <LinearGradient
+        onPress={handleSubmit}
+        // Button Linear Gradient
+        colors={["#ffbf00", "#FFCC33", "#FFE085"]}
+        style={{
+          // start: ["-1", "-2"],
+          // end: ["-2", "-2"],
+          marginTop: 10,
+          marginBottom: 10,
+          padding: 16,
+          alignItems: "center",
+          borderRadius: 28,
+          alignSelf: "stretch",
+          alignItems: "center",
+        }}
+      >
+        <AuthButtonText>Sign In</AuthButtonText>
+      </LinearGradient>
       {isValidUser ? (
         <Text style={{ color: "green" }}> forget password </Text>
       ) : (
-        <Animatable.View animation="fadeInLeft" duration={400}>
+        <Animatable.View animation='fadeInLeft' duration={400}>
           <Text style={{ color: "red" }}>Invalid username or password </Text>
         </Animatable.View>
       )}
-
-      <AuthButton onPress={handleSubmit}>
-        <AuthButtonText>Sign in</AuthButtonText>
-      </AuthButton>
       <AuthOther onPress={() => navigation.navigate("Signup")}>
-        Click here to register!
+        Click Here to Register!
       </AuthOther>
     </AuthContainer>
   );
